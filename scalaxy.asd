e4c81fb07cf6fda5b12c2534452eda1f4951edc5
Artem Andreenko <miolini@users.noreply.github.com>
2026-08-06 18:06:24 -0700

    Bump version to 1.6.7

--- scalaxy.asd
+++ scalaxy.asd
@@ -5,7 +5,7 @@
 
 (defsystem "scalaxy"
   :description "Scalaxy: multi-purpose cloud-ready distributed database."
-  :version "0.1.0"
+  :version "1.6.7"
   :author "Artem Andreenko <miolini>"
   :license "MIT"
   :serial t
--- src/web.lisp
+++ src/web.lisp
@@ -9,7 +9,7 @@
 
 (defvar *web-asset-cache* (make-hash-table :test #'equal))
 
-(defparameter +version+ "0.1.0")
+(defparameter +version+ "1.6.7")
 
 (defun web-read-asset (web-dir rel)
   "Read an asset from WEB-DIR, cached in memory."
